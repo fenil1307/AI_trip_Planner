71b9a58921d17761292647a3be805e87afb9f5c8
fenil kheni <fenilkheni1307@gmail.com>
2025-06-28 10:52:28 +0530

    files updated

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -6,6 +6,7 @@ from utils.config_loader import load_config
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 
+
 class ConfigLoader:
     def __init__(self):
         print(f"Loaded config.....")
